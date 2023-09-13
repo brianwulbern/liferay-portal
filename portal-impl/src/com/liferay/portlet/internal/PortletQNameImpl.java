f9307e86c190e0014ae81b67d75961cc90fc578a
Piyush Gupta <117366639+Piyush-Gupta10@users.noreply.github.com>
2023-09-13 09:10:40 +0530

    Revert "Auxiliary commit to revert individual files from 8647357b6c57ce039003aa4e466814af009f2177"
    This reverts commit 7d0362b4bb3fb994cc6faf6b24eec32cb382bebc.

--- portal-impl/src/com/liferay/portlet/internal/PortletQNameImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/PortletQNameImpl.java
@@ -145,7 +145,8 @@ public class PortletQNameImpl implements PortletQName {
 		String publicRenderParameterName, String identifier) {
 
 		if (!Objects.equals(
-				identifier, _identifiers.get(publicRenderParameterName))) {
+				identifier,
+				_identifiers.get(publicRenderParameterName))) {
 
 			_identifiers.put(publicRenderParameterName, identifier);
 		}
